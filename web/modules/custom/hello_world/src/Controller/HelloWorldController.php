533564df2ce158c259a8d27cfd620fdd7f1501cb
Benedict <benedict.omare@un.org>
2022-02-07 22:54:05 +0100

    inject service into controller. Replace basic markup with get

--- web/modules/custom/hello_world/src/Controller/HelloWorldController.php
+++ web/modules/custom/hello_world/src/Controller/HelloWorldController.php
@@ -3,21 +3,51 @@
 namespace Drupal\hello_world\Controller;
 
 use Drupal\Core\Controller\ControllerBase;
+use Drupal\hello_world\HelloWorldSalutation;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
  * Controller for the salutation message.
  */
 class HelloWorldController extends ControllerBase {
 
+     /**
+   * The salutation service.
+   *
+   * @var \Drupal\hello_world\HelloWorldSalutation
+   */
+  protected $salutation;
+
+  /**
+   * HelloWorldController constructor.
+   *
+   * @param \Drupal\hello_world\HelloWorldSalutation $salutation
+   *   The salutation service.
+   */
+  public function __construct(HelloWorldSalutation $salutation) {
+    $this->salutation = $salutation;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container) {
+    return new static(
+      $container->get('hello_world.salutation')
+    );
+  }
+
     /**
-     * Hello world.
+     * Hello World.
      *
      * @return array
-     *  Our message.
+     *  Our message. 
      */
     public function helloWorld() {
         return [
-            '#markup' => $this->t('Hello World'),
+            /* '#markup' => $this->t('Hello World'), */
+            '#markup' => $this->salutation->getSalutation(),
         ];
         }
+        
     }
